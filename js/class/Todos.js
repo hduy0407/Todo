cec4c89fa648ac2734e1077a9f96534e1038e81c
hduy0407 <t3ngdu01@students.oamk.fi>
2024-03-24 04:19:09 +0200

    Part 5

--- js/class/Todos.js
+++ js/class/Todos.js
@@ -1,8 +1,6 @@
 
 import {Task} from "./Task.js"
 
-
-
 class Todos {
     #tasks = []
     #backend_url = ''
@@ -41,6 +39,21 @@ class Todos {
         })
     }
 
+    removeTask = (id) => {
+        return new Promise(async(resolve, reject) => {
+            fetch(this.#backend_url + '/delete/' + id,{
+                method: 'delete'
+            })
+            .then((response) => response.json())
+            .then((json) => {
+                this.#removeFromArray(id)
+                resolve(json.id)
+            }, (error) => {
+                reject(error)
+            })
+        })
+    }
+
     #readJson = (taskAsJson) => {
         taskAsJson.forEach(node => {
             const task = new Task(node.id, node.description)
@@ -52,6 +65,11 @@ class Todos {
         const task = new Task(id, text)
         this.#tasks.push(task)
         return task
+    }
+
+    #removeFromArray = (id) => {
+        const arrayWithoutRemoved = this.#tasks.filter(task => task.id !== id)
+        this.#tasks = arrayWithoutRemoved
     }
 } 
 
